3f9de4b0a20e12c69fd6e8bdc341638a3fbb2df4
BodyConnectionNutrition <webb.nutrition@gmail.com>
2026-08-22 16:42:32 -0700

    Make bundle checkout environment independent

--- netlify/functions/create-tool-checkout.js
+++ netlify/functions/create-tool-checkout.js
@@ -15,7 +15,7 @@ exports.handler = async event => {
   else if (config.amount) {
     body.set("line_items[0][price_data][currency]", "usd");
     body.set("line_items[0][price_data][unit_amount]", String(config.amount));
-    body.set("line_items[0][price_data][product_data][name]", product);
+    body.set("line_items[0][price_data][product_data][name]", config.name || product);
     body.set("line_items[0][price_data][product_data][metadata][product_slug]", product);
   } else return { statusCode: 500, body: "Checkout price is not configured." };
   body.set("line_items[0][quantity]", "1");
